0a520380e6d35697a3e7429fc58a049d739694e3
Roibruler <adamroy7428@gmail.com>
2026-09-17 21:45:29 -0400

    retirer plugin swagger, causait un problème, on veut juste les property

--- src/appreciation/dto/create-appreciation.dto.ts
+++ src/appreciation/dto/create-appreciation.dto.ts
@@ -1,19 +1,24 @@
 import { IsString, IsNotEmpty, IsInt, Min, Max } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateAppreciationDto {
+    @ApiProperty({ example: "loc_3fa85f64-5717-4562-b3fc-2c963f66afa6" })
     @IsString()
     @IsNotEmpty()
     placeId: string;
 
+    @ApiProperty({ example: "Samira" })
     @IsString()
     @IsNotEmpty()
     authorName: string;
 
+    @ApiProperty({ example: 4, minimum: 1, maximum: 5 })
     @IsInt()
     @Min(1)
     @Max(5)
     rating: number;
 
+    @ApiProperty({ example: "Calme et Wi-Fi stable." })
     @IsString()
     @IsNotEmpty()
     comment: string;
